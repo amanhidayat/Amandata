fbd4b1c435800a15445ac51e8ac270d768a5993a
amanhidayat <amanhidayat39@gmail.com>
2023-09-01 09:22:59 +0700

    four

---
+++ array.js
@@ -0,0 +1,17 @@
+let arr = ["a", "b", "c", "d", "e"]
+
+let arr2 = new Array("a", "b", "c", "d", "e")
+
+console.log(arr);
+console.log(arr2);
+
+console.log(arr[2]) // "c"
+console.log(arr2[3]);
+
+let data = [1, 2, 3]
+
+data.push(4)
+console.log(data);
+
+data.unshift(0)
+console.log(data);
